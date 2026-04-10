aac4a3d19a4c44f45491cb76a064049f68bdd1d7
ayznde <ayznde.off@gmail.com>
2026-04-10 10:19:30 +0300

    deleted dublicated files

--- common.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <string>
-
-#include <pqxx/pqxx> // Подключение к БД 
-#include <sodium.h> // Хэширование паролей (Argon2)
-#include <stdexcept> // Исключения для ошибок
-#include <limits>// Лимиты числовых типов
