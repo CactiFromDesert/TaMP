6f779b278744b8eaebec1fee3f567018198d09f7
CactiFromDesert <CactiFromDesert@example.comC>
2026-04-17 12:55:35 +0300

    UpdateClientAndCreateServer

--- Client_Server/TCPClient.hpp
+++ Client_Server/TCPClient.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #ifndef TCP_CLIENT_H 
 #define TCP_CLIENT_H 
 
---
+++ Client_Server/TCPServer.cpp
@@ -0,0 +1,70 @@
+#include "TCPServer.hpp"
+
+std::string TCPServer::Message;
+
+void* TCPServer::Task(void *arg)
+{
+    int n;
+    int newsockfd = (long)arg;
+    char msg[MAXPACKETSIZE];
+    pthread_detach(pthread_self());
+    while(1)
+    {
+        n = recv(newsockfd, msg, MAXPACKETSIZE, 0);
+        if(n <= 0)
+        {
+            close(newsockfd);
+            break;
+        }
+        msg[n] = 0;
+        Message = std::string(msg);
+        // send(newsockfd, msg, n, 0);
+    }
+    return 0;
+}
+
+void TCPServer::setup(int port)
+{
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(port);
+    bind(sockfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    listen(sockfd, 5);
+}
+
+std::string TCPServer::receive()
+{
+    std::string str;
+    while(1)
+    {
+        socklen_t sosize = sizeof(clientAddress);
+        newsockfd = accept(sockfd, (struct sockaddr*)&clientAddress, &sosize);
+        str = inet_ntoa(clientAddress.sin_addr);
+        pthread_create(&serverThread, NULL, &Task, (void*)(long)newsockfd);
+        return str;  // Возвращаем IP адрес клиента
+    }
+}
+
+std::string TCPServer::getMessage()
+{
+    return Message;
+}
+
+void TCPServer::Send(std::string msg)
+{
+    send(newsockfd, msg.c_str(), msg.length(), 0);
+}
+
+void TCPServer::clean()
+{
+    memset(msg, 0, MAXPACKETSIZE);
+    Message = "";
+}
+
+void TCPServer::detach()
+{
+    close(sockfd);
+    close(newsockfd);
+}
